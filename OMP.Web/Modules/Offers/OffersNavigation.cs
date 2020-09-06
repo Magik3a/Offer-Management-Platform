de912fdc0844ece08e08c3153da650a7426797f6
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2020-09-06 15:42:00 +0300

    Reordered Offer Navigation

--- OMP.Web/Modules/Offers/OffersNavigation.cs
+++ OMP.Web/Modules/Offers/OffersNavigation.cs
@@ -1,13 +1,17 @@
 ﻿using Serenity.Navigation;
 using MyPages = OMP.Offers.Pages;
 
-[assembly: NavigationLink(int.MaxValue, "Offers/Countries", typeof(MyPages.CountriesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Cities", typeof(MyPages.CitiesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Accounts", typeof(MyPages.AccountsController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Companies", typeof(MyPages.CompaniesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Offer Statuses", typeof(MyPages.OfferStatusesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Offers", typeof(MyPages.OffersController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Task Statuses", typeof(MyPages.TaskStatusesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Categories", typeof(MyPages.CategoriesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Offer Categories", typeof(MyPages.OfferCategoriesController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Offers/Offer Category Tasks", typeof(MyPages.OfferCategoryTasksController), icon: null)]
+[assembly: NavigationLink(1000, "Offers", typeof(MyPages.OffersController), icon: "fa-file-text-o")]
+                      
+[assembly: NavigationMenu(1000, "Organization", icon: "fa-sitemap")]
+[assembly: NavigationLink(1000, "Organization/Countries", typeof(MyPages.CountriesController), icon: null)]
+[assembly: NavigationLink(1000, "Organization/Cities", typeof(MyPages.CitiesController), icon: null)]
+[assembly: NavigationLink(1000, "Organization/Accounts", typeof(MyPages.AccountsController), icon: null)]
+[assembly: NavigationLink(1000, "Organization/Companies", typeof(MyPages.CompaniesController), icon: null)]
+                      
+[assembly: NavigationMenu(1000, "Offers Settings", icon: "fa-folder-o")]
+[assembly: NavigationLink(1000, "Offers Settings/Categories", typeof(MyPages.CategoriesController), icon: null)]
+[assembly: NavigationLink(1000, "Offers Settings/Offer Statuses", typeof(MyPages.OfferStatusesController), icon: null)]
+[assembly: NavigationLink(1000, "Offers Settings/Task Statuses", typeof(MyPages.TaskStatusesController), icon: null)]
+[assembly: NavigationLink(1000, "Offers Settings/Offer Categories", typeof(MyPages.OfferCategoriesController), icon: null)]
+[assembly: NavigationLink(1000, "Offers Settings/Offer Category Tasks", typeof(MyPages.OfferCategoryTasksController), icon: null)]
